d4ecb2b0ff61aabbe21ac8c3767bf22f1b210005
vivekrawat1998 <vr90888561@gmail.com>
2025-02-06 10:49:53 +0530

    thi is my commit

--- app.js
+++ app.js
@@ -3,26 +3,29 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const cookieparser = require('cookie-parser');
-const userRouter = require("./routes/user.routes")
+const userRouter = require("./routes/user.routes");
 const captainRouter = require('./routes/captain.routes');
 const mapRouter = require('./routes/maps.routes');
-const riderouter = require("./routes/ride.routes")
+const riderouter = require("./routes/ride.routes");
 const path = require('path');
-
 const cors = require('cors');
-const connectDB = require('./db/db'); 
+const connectDB = require('./db/db');
+
 app.use(cors());
-
 app.use(cookieparser());
-connectDB();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectDB();
 
 app.use('/users', userRouter);
 app.use('/captains', captainRouter);
 app.use('/maps', mapRouter);
 app.use('/ride', riderouter);
+
+app.get('/health', (req, res) => {
+  res.status(200).send('Server is healthy');
+});
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -30,13 +33,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-app.get('/health', (req, res) => {
-  res.status(200).send('Server is healthy');
-});
-
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
-
 
 module.exports = app;
