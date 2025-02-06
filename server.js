b3e42a07c684e7ba5fd9c00e5a2954358d665810
vivekrawat1998 <vr90888561@gmail.com>
2025-02-06 10:46:47 +0530

    thi is the commit

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ io.on('connection', (socket) => {
   console.log('New WebSocket connection');
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
